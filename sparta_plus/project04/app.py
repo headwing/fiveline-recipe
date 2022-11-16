163b2e9d5abea58692db27fd6b4247197e89db99
headwing <110597846+headwing@users.noreply.github.com>
2022-11-17 04:02:50 +0900

    병합

--- sparta_plus/project04/app.py
+++ sparta_plus/project04/app.py
@@ -191,13 +191,8 @@ def comment_post():
     comment_receive = request.form['comment_give']
     title_receive = request.form['title_give']
     doc = {
-<<<<<<< HEAD
-        'title': title_receive,
-        'comment': comment_receive
-=======
         'comment': comment_receive,
         'title': title_receive
->>>>>>> e18ddeef31be8e39ace268935476f046d3e9b2c0
     }
     db.comment.insert_one(doc)
     return jsonify({'msg': 'POST 게시 완료!'})
@@ -216,6 +211,7 @@ def post_get():
     title_receive = str(request.form['title_give'])
     posting_list = db.post.find_one({'recipe': title_receive}, {'_id': False})
     return jsonify({'present': posting_list})
+
 @app.route("/comment", methods=["POST"])
 def comment_get():
     title_receive = str(request.form['title_give'])
